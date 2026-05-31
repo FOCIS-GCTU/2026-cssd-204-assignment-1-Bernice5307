3be91dff566179ad4736a428f4772433c8147a1f
Bernice5307 <2425403208@live.gctu.edu.gh>
2026-05-31 18:52:21 +0000

    Assignment 1 done

--- initials.py
+++ initials.py
@@ -1,6 +1,59 @@
+ #File: initials.py
+
+ #Description : Print out my initials.
+
+ #Assignment Number: 1
+
+ #
+
+ #Name: Bernice Boadi Kodua
+
+ #STUDENT ID: 2425403208
+
+ #Email: 2425403208@live.gctu.edu.gh
+
+ #Grader: Augustus
+
+ #
+
+ #On my honour, Bernice Boadi Kodua, this programming assignment is my own work
+
+ #and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+
+     #Print my initials in large stylized letters.
+
+    print()
+
+    print("...BBK")
+
+    print()
+
+    print("...BBBBBBBBBBBB........BBBBBBBBBBBB........KKK......KKK.....")
+
+    print("...BBB.......BB........BBB.......BB........KKK.....KKK......")
+
+    print("...BBB.......BB........BB........BB........KKK....KKK.......")
+
+    print("...BBBBBBBBBBBB........BBBBBBBBBBBB........KKK...KKK........")
+
+    print("...BBBBBBBBBBBB........BBBBBBBBBBBB........KKKKKKKK.........")
+
+    print("...BBB.......BB........BB........BB........KKK....KKK.......")
+
+    print("...BBB.......BB........BBB.......BB........KKK.....KKK......")
+
+    print("...BBB.......BB........BBB.......BB........KKK.....KKK......")
+
+    print("...BBB.......BB...**...BBB.......BB...**...KKK......KKK...**")
+
+    print("...BBBBBBBBBBBB...**...BBBBBBBBBBBB...**...KKK......KKK...**")
+
+    print()
+
+print ()
+
 
 
 
